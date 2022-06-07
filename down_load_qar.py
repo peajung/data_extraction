8afea7337f00aae451d1f1830ba3fad116f02a31
Pakapone Supavarn <peajung@gmail.com>
2022-06-07 11:12:29 +0700

    modiry download qar py

--- down_load_qar.py
+++ down_load_qar.py
@@ -1,8 +1,7 @@
 # import libraries
 from urllib.request import urlretrieve
-from datetime import datetime
-import pandas as pd
 import bz2
+from os import remove
 
 # download flight plan zipfile and unzip
 flight_plan_url = 'http://sfuelepcn1.thaiairways.co.th/etl/latest/qars.json.bz2'
@@ -13,3 +12,6 @@ zipfile = bz2.BZ2File('qar.json.bz2') # open the file
 data = zipfile.read() # get the decompressed data
 newfilepath = 'C:/Users/peaju/THAI AIRWAYS INTERNATIONAL PUBLIC CO.,LTD/DP - PC - Team 1 - Documents/PC - Team 1/2022/python/data_extraction/data/qar.json.bz2'[:-4] # assuming the filepath ends with .bz2
 open(newfilepath, 'wb').write(data) # write a uncompressed file
+zipfile.close()
+
+remove('qar.json.bz2')
